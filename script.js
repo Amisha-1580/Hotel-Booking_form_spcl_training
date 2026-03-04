c6f44eaad8a0a624188bd9bdffe6328922964fc7
Amisha Bhasme <originwork520@gmail.com>
2026-03-04 06:47:23 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Keep JS minimal for now, just test submit alert
 document.getElementById("bookingForm").addEventListener("submit", function(event) {
   event.preventDefault();
   alert("Form submitted successfully!");
