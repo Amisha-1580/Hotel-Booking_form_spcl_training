dca5f5cd7faeba1b089aacb427889d53b81cae8c
Amisha Bhasme <originwork520@gmail.com>
2026-03-06 18:30:40 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-// Keep JS minimal for now, just test submit alert
-document.getElementById("bookingForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  alert("Form submitted successfully!");
+const form = document.getElementById("bookingForm");
+const checkin = document.getElementById("checkin");
+const checkout = document.getElementById("checkout");
+const message = document.getElementById("message");
+
+form.addEventListener("submit", function(e){
+
+e.preventDefault();
+
+let checkinDate = new Date(checkin.value);
+let checkoutDate = new Date(checkout.value);
+let today = new Date();
+
+today.setHours(0,0,0,0);
+
+if(checkinDate < today){
+message.textContent = "Check-in date cannot be in the past";
+message.style.color = "red";
+return;
+}
+
+if(checkoutDate <= checkinDate){
+message.textContent = "Check-out must be after check-in date";
+message.style.color = "red";
+return;
+}
+
+message.textContent = "Dates are valid!";
+message.style.color = "green";
+
 });
